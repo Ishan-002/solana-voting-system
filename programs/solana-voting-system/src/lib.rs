85851039ee2a3c85e302dbdefa6e4d77e05fa95e
Restyled.io <commits@restyled.io>
2022-06-06 22:09:55 +0000

    Restyled by rustfmt

--- programs/solana-voting-system/src/lib.rs
+++ programs/solana-voting-system/src/lib.rs
@@ -7,7 +7,6 @@ declare_id!("9tzc763yy5v74LKwwjRdUwJyB1mQMxcrgVntpehcnm5H");
 pub mod solana_voting_system {
     use super::*;
 
-
     pub fn send_vote(ctx: Context<SendVote>) -> Result<()> {
         let chosen = ctx.accounts.option.option;
         let mut updatectx: Context<UpdateCount>;
@@ -17,17 +16,15 @@ pub mod solana_voting_system {
         Ok(())
     }
 
-    pub fn update_count(ctx: Context<UpdateCount>, option: usize) -> Result<()>{
-        ctx.accounts.full_count.vote_vec[option] =  ctx.accounts.full_count.vote_vec[option] + 1;
-        Ok(()) 
+    pub fn update_count(ctx: Context<UpdateCount>, option: usize) -> Result<()> {
+        ctx.accounts.full_count.vote_vec[option] = ctx.accounts.full_count.vote_vec[option] + 1;
+        Ok(())
     }
 
-    pub fn add_option(ctx: Context<AddOption>, input: &String) -> Result<()>{
-
+    pub fn add_option(ctx: Context<AddOption>, input: &String) -> Result<()> {
         Ok(())
     }
 }
-
 
 #[derive(Accounts)]
 pub struct SendVote<'info> {
@@ -35,33 +32,32 @@ pub struct SendVote<'info> {
     pub option: Account<'info, Option>,
     #[account(mut)]
     pub author: Signer<'info>,
-    pub system_program: AccountInfo<'info>
+    pub system_program: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
 pub struct AddOption<'info> {
-    pub new_candidate: Account<'info, Candidate>
+    pub new_candidate: Account<'info, Candidate>,
 }
 
 #[derive(Accounts)]
-pub struct UpdateCount<'info>{
-    pub full_count: Account<'info, AllOptionsVotes>
-
+pub struct UpdateCount<'info> {
+    pub full_count: Account<'info, AllOptionsVotes>,
 }
 
 #[account]
-pub struct AllOptionsVotes{
+pub struct AllOptionsVotes {
     pub option_vec: Vec<String>,
     pub vote_vec: Vec<i64>,
-    pub count: u64
+    pub count: u64,
 }
 
 #[account]
-pub struct Option{
-    pub option: usize
+pub struct Option {
+    pub option: usize,
 }
 
 #[account]
-pub struct Candidate{
-    pub candidate: String
+pub struct Candidate {
+    pub candidate: String,
 }
